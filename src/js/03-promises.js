b5f71208ffd3631185eeeb2f81ff5a6f6df88692
AndrosenkoArtem <yixyxtstt@gmail.com>
2023-04-02 07:54:15 +0300

    Update 03-promises.js
    broken promiseCreate into functions

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,9 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
-const refs = {
-  form: document.querySelector('.form'),
-  delay: document.getElementsByName('delay'),
-  step: document.getElementsByName('step'),
-  amount: document.getElementsByName('amount'),
-};
-let currentPosition = 0;
-let promiseResultInterval = null;
+const form = document.querySelector('.form');
+
 let isActive = false;
-// let currentDelay = 0;
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -18,10 +11,10 @@ function createPromise(position, delay) {
     setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
-        resolve({ position, delay });
+        resolve(position);
       } else {
         // Reject
-        reject({ position, delay });
+        reject(position);
       }
     }, delay);
   });
@@ -29,40 +22,100 @@ function createPromise(position, delay) {
 }
 function onSubmitForm(e) {
   e.preventDefault();
-  let currentDelay = Number(refs.delay[0].value);
-
+  const deleyInputValue = e.currentTarget.elements.delay.value;
+  const stepInputValue = e.currentTarget.elements.step.value;
+  const amountInputValue = e.currentTarget.elements.amount.value;
+  let currentPosition = 0;
+  let currentDelay = Number(deleyInputValue);
   if (isActive) {
     return;
   }
   isActive = true;
-  promiseResultInterval = setInterval(() => {
+
+  const promiseResultInterval = setInterval(() => {
     currentPosition += 1;
-    if (currentPosition > Number(refs.amount[0].value)) {
-      clearInterval(promiseResultInterval);
-      isActive = false;
-      currentPosition = 0;
+    ifPromiseIntervalCompleted({
+      promiseResultInterval,
+      currentPosition,
+      amountInputValue,
+    });
 
-      return;
-    }
-
-    createPromise(currentPosition, refs.delay[0].value)
-      .then(({ position, delay }) => {
-        currentDelay += Number(refs.step[0].value);
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${
-            currentDelay - Number(refs.step[0].value)
-          }ms`
-        );
+    createPromise(currentPosition, deleyInputValue)
+      .then(position => {
+        currentDelay += Number(stepInputValue);
+        createPrimiseSuccsess({ currentDelay, stepInputValue, position });
       })
-      .catch(({ position, delay }) => {
-        currentDelay += Number(refs.step[0].value);
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${
-            currentDelay - Number(refs.step[0].value)
-          }ms`
-        );
+      .catch(position => {
+        currentDelay += Number(stepInputValue);
+        createPromiseFailed({ currentDelay, stepInputValue, position });
       });
-  }, refs.step[0].value);
+  }, stepInputValue);
 }
-refs.form.addEventListener('submit', onSubmitForm);
+function ifPromiseIntervalCompleted({
+  promiseResultInterval,
+  currentPosition,
+  amountInputValue,
+}) {
+  if (currentPosition >= amountInputValue) {
+    clearInterval(promiseResultInterval);
+    isActive = false;
+    currentPosition = 0;
+
+    return;
+  }
+}
+function createPrimiseSuccsess({ currentDelay, stepInputValue, position }) {
+  Notiflix.Notify.success(
+    `✅ Fulfilled promise ${position} in ${
+      currentDelay - Number(stepInputValue)
+    }ms`
+  );
+}
+function createPromiseFailed({ currentDelay, stepInputValue, position }) {
+  // currentDelay += Number(stepInputValue);
+  Notiflix.Notify.failure(
+    `❌ Rejected promise ${position} in ${
+      currentDelay - Number(stepInputValue)
+    }ms`
+  );
+}
+
+form.addEventListener('submit', onSubmitForm);
 //
+// function onSubmitForm(e) {
+//   e.preventDefault();
+//   let currentDelay = Number(refs.delay[0].value);
+
+//   if (isActive) {
+//     return;
+//   }
+//   isActive = true;
+//   promiseResultInterval = setInterval(() => {
+//     currentPosition += 1;
+//     if (currentPosition > Number(refs.amount[0].value)) {
+//       clearInterval(promiseResultInterval);
+//       isActive = false;
+//       currentPosition = 0;
+
+//       return;
+//     }
+
+//     createPromise(currentPosition, refs.delay[0].value)
+//       .then(({ position, delay }) => {
+//         currentDelay += Number(refs.step[0].value);
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${
+//             currentDelay - Number(refs.step[0].value)
+//           }ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         currentDelay += Number(refs.step[0].value);
+//         Notiflix.Notify.failure(
+//           `❌ Rejected promise ${position} in ${
+//             currentDelay - Number(refs.step[0].value)
+//           }ms`
+//         );
+//       });
+//   }, refs.step[0].value);
+// }
